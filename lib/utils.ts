adbababaa415c09066259c2f24d2cd3c2568f12b
Albin Lamichhane <albin.lamichhane@tuni.fi>
2024-02-20 12:56:23 +0200

    added models and schemas

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,34 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// ERROR HANDLER
+export const handleError = (error: unknown) => {
+  switch (typeof error) {
+    case "object":
+      // This is a native JavaScript error (e.g., TypeError, RangeError)
+      if (error instanceof Error) {
+        console.error(error.message);
+        throw new Error(`Error: ${error.message}`);
+      }
+      // This is an unknown type of error
+      else {
+        console.error(error);
+        throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+      }
+      break;
+    case "string":
+      // This is a string error message
+      console.error(error);
+      throw new Error(`Error: ${error}`);
+      break;
+    default:
+      // This is an invalid type of error
+      console.error(error);
+      throw new Error(`Invalid error: ${error}`);
+      break;
+  }
+};
