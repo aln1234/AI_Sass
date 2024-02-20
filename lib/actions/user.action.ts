45b388b06ed06f66874f274fa3a7b978b4dc0f79
Albin Lamichhane <albin.lamichhane@tuni.fi>
2024-02-20 13:00:03 +0200

    added qs package

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -33,41 +33,41 @@ export async function getUserById(userId: string) {
   }
 }
 
-// UPDATE
-export async function updateUser(clerkId: string, user: UpdateUserParams) {
-  try {
-    await connectToDatabase();
+// // UPDATE
+// export async function updateUser(clerkId: string, user: UpdateUserParams) {
+//   try {
+//     await connectToDatabase();
 
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
-      new: true,
-    });
+//     const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+//       new: true,
+//     });
 
-    if (!updatedUser) throw new Error("User update failed");
+//     if (!updatedUser) throw new Error("User update failed");
 
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    handleError(error);
-  }
-}
+//     return JSON.parse(JSON.stringify(updatedUser));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
 
-// DELETE
-export async function deleteUser(clerkId: string) {
-  try {
-    await connectToDatabase();
+// // DELETE
+// export async function deleteUser(clerkId: string) {
+//   try {
+//     await connectToDatabase();
 
-    // Find user to delete
-    const userToDelete = await User.findOne({ clerkId });
+//     // Find user to delete
+//     const userToDelete = await User.findOne({ clerkId });
 
-    if (!userToDelete) {
-      throw new Error("User not found");
-    }
+//     if (!userToDelete) {
+//       throw new Error("User not found");
+//     }
 
-    // Delete user
-    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-    revalidatePath("/");
+//     // Delete user
+//     const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+//     revalidatePath("/");
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-  } catch (error) {
-    handleError(error);
-  }
-}
+//     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
