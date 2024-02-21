589a66eb0058821f987fa9869e4c0433929d8d3e
Albin <albin@Haris-MacBook-Pro.local>
2024-02-21 12:25:05 +0200

    added header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,28 @@
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AddTransformationsTypePage = () => {
-  return <div>AddTransformationsTypePage</div>;
+const AddTransformationsTypePage = async({params:{type}}:SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  const { userId } = auth();
+  if(!userId) redirect("/sign-in")
+
+  const user = await getUserById(userId);
+  
+  
+  return (
+    <>
+    
+    <Header title={transformation.title} subtitle={transformation.subTitle}/>
+    <section className="mt-10">
+      hello 
+    </section>
+    </>
+  )
 };
 
 export default AddTransformationsTypePage;
